5d8b0b5dd72890ea65c9a693244436f96e107d66
Igor Khvostenkov <Igor.Khvostenkov@logmein.com>
2019-02-11 17:15:49 +0100

    evtStateChanged added with 4

--- device_darwin.go
+++ device_darwin.go
@@ -377,7 +377,8 @@ func (d *device) HandleXpcEvent(event xpc.Dict, err error) {
 
 	switch id {
 	case // device event
-		5,  // StateChanged
+		4,
+		6,  // StateChanged
 		16, // AdvertisingStarted
 		17, // AdvertisingStopped
 		18: // ServiceAdded
